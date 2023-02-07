8aef5e3f1e63bf30e1bf5b383ce7f110e4b5037c
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2023-02-07 01:07:57 -0500

    Add files via upload
    Create tables: Patient, Doctor, Appointment, Finance

---
+++ Assignment 1 Hospital.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `Patient`(
+  `patientID` int AUTO_INCREMENT NOT Null PRIMARY KEY,
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL,
+  `age` int NOT NULL,
+  `phoneNumber` int NOT NULL,
+  `diagnosis` varchar(100) NOT NULL,
+  `hospitalBill` int NOT NULL,
+  `surgeryType` varchar(50) NOT NULL,
+  `assignedDoctorID` int NOT NULL,
+  `appointmentNumber` int NOT NULL
+);
+
+CREATE TABLE `Appointment` (
+  `appointmentNumber` int NOT NULL AUTO_INCREMENT,
+  `doctorID` int NOT NULL,
+  `patientID` int NOT NULL,
+  `Diagnosis` varchar(100) NOT NULL,
+  `prescription` varchar(100) NOT NULL,
+  PRIMARY KEY (`appointmentNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Doctor` (
+  `doctorID` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `department` varchar(45) NOT NULL,
+  `appointmentNumber` int NOT NULL,
+  PRIMARY KEY (`doctorID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Finance` (
+  `treatmentID` varchar(45) NOT NULL,
+  `treatmentCost` int NOT NULL,
+  `Treatment` varchar(45) NOT NULL,
+  PRIMARY KEY (`treatmentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
