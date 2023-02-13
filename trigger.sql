358bc6ba04fe31345b541ce43bf1634d7d4b7f4f
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2023-02-12 23:43:17 -0500

    Trigger & Trigger Test added.

--- trigger.sql
+++ trigger.sql
@@ -1,4 +1,8 @@
-CREATE TRIGGER `Total Cost`
-AFTER INSERT ON
-Patient
-SELECT SUM(treatmentCost);
+Insert IGNORE INTO Patient (patientID,firstName,lastName,age,phoneNumber,diagnosis,treatmentCost,treatment,doctorID,appointmentNumber)
+VALUES (2023009, 'Trigger', 'One', '01', 8761234567, 'Appendicitis', 150000, 'Appendectomy', 1217000, 1617009);
+
+CREATE TRIGGER `Increment age`
+AFTER INSERT ON Patient  
+FOR EACH ROW
+SET age = age + 1;
+
